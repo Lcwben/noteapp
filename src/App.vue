ba032f28ba283f5bab4941b243ae6b8ff91562ec
lcwben <lcwben@126.com>
2020-03-07 22:00:27 +0800

    feat1: make side menu: init side menu
    1.refomat code style. 2.import vue-router. 3.make menu frame.

--- src/App.vue
+++ src/App.vue
@@ -7,25 +7,25 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Menu from './components/side/Menu.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import Menu from './components/side/Menu.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-    Menu
+  export default {
+    name: 'App',
+    components: {
+      HelloWorld,
+      Menu
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+
+Vue.use(VueRouter);
+Vue.use(ViewUI)
 
 Vue.config.productionTip = false
 
